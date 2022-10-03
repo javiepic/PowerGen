e449a8ff47602724401964e42e07a990f0f5f65c
Javier Solis <111769866+javiepic@users.noreply.github.com>
2022-10-03 02:26:23 +0000

    1

--- .ccls-cache/@home@runner@PowerGeneration/main.cpp
+++ .ccls-cache/@home@runner@PowerGeneration/main.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 const float  g = 9.81;
 //Functions
-double powerGen(double, int, int, int, double );
+int powerGen(double, int, int, int, double );
 
 int main() {
   double P;
-  powerGen(double P);
-    
+  powerGen(P);
+
+  return 0;
 }
-double powerGen(double P, int p, int Q, double H)
+int powerGen(double P, int p, int Q, double H)
 {
   cin >> P >> p >> Q >> H;
   P = p * Q * H * g;
   
-  
+  return P;
 }
